efe90d768894f2fad3842d4cb49189c3b9878eb5
Denis_L <denis88lyapin@gmail.com>
2023-06-04 14:40:37 +0300

    created test LinkedList to test_linked_list.py

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,25 @@
-"""Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+from src.linked_list import LinkedList
+
+
+class LinkedListTestCase(unittest.TestCase):
+    def test_linked_list(self):
+        # Создаем пустой список
+        ll = LinkedList()
+
+        # Проверяем пустой список
+        self.assertEqual(str(ll), "None")
+
+        # Добавляем узлы в начало списка
+        ll.insert_beginning({'id': 1})
+        ll.insert_beginning({'id': 0})
+
+        # Проверяем список после добавления узлов в начало
+        self.assertEqual(str(ll), "{'id': 0} -> {'id': 1} -> None")
+
+        # Добавляем узлы в конец списка
+        ll.insert_at_end({'id': 2})
+        ll.insert_at_end({'id': 3})
+
+        # Проверяем список после добавления узлов в конец
+        self.assertEqual(str(ll), "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None")
