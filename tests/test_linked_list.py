cd6d7f29bb33394c15e79f52f5adb7f3bc8821dc
Denis_L <denis88lyapin@gmail.com>
2023-06-08 21:12:28 +0300

    added tests to_list and get_data_by_id in test_linked_list.py

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -23,3 +23,37 @@ class LinkedListTestCase(unittest.TestCase):
 
         # Проверяем список после добавления узлов в конец
         self.assertEqual(str(ll), "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None")
+
+    def test_get_data_by_id(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        ll.insert_at_end('idusername')
+        ll.insert_at_end([1, 2, 3])
+        ll.insert_at_end({'id': 2, 'username': 'mosh_s'})
+
+        result = ll.get_data_by_id(2)
+        expected = {'id': 2, 'username': 'mosh_s'}
+        self.assertEqual(result, expected)
+
+    def test_to_list(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        ll.insert_at_end({'id': 2, 'username': 'mik.roz'})
+        ll.insert_at_end({'id': 3, 'username': 'mosh_s'})
+
+        result = ll.to_list()
+        expected = [{'id': 1, 'username': 'lazzy508509'}, {'id': 2, 'username': 'mik.roz'},
+                    {'id': 3, 'username': 'mosh_s'}]
+        self.assertEqual(result, expected)
+
+    def test_get_data_by_id_invalid_data(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        ll.insert_at_end('idusername')
+        ll.insert_at_end([1, 2, 3])
+        ll.insert_at_end({'id': 2, 'userna'
+                                   'me': 'mosh_s'})
+
+        result = ll.get_data_by_id(4)
+        expected = None
+        self.assertEqual(result, expected)
